bdaaab7a5fbbfa693c1629c94debba2daab697b2
Rónán <ronan_mccabe@protonmail.com>
2021-03-31 16:22:17 -0400

    Change tip count copy

--- src/elm/View/Post.elm
+++ src/elm/View/Post.elm
@@ -59,7 +59,7 @@ view dProfile timestamp now replies accounting state tooltipState topic wallet p
                                             txt =
                                                 case val.labelType of
                                                     Tip ->
-                                                        "Crowd tipped the author $" ++ Misc.formatDollar data.totalTipped
+                                                        "The author has been tipped $" ++ Misc.formatDollar data.totalTipped ++ " for this post"
 
                                                     Burn ->
                                                         "Author burned $" ++ Misc.formatDollar post.authorBurn ++ " + crowd amplified $" ++ Misc.formatDollar (TokenValue.sub data.totalBurned post.authorBurn)
