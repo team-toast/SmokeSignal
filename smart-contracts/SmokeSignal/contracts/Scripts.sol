0df6f867a98d400a6b7f9410fd14b99597076022
coinop-logan <coinop.logan@gmail.com>
2021-06-03 14:34:14 +0000

    Redeploy fixed scripts contract

--- smart-contracts/SmokeSignal/contracts/Scripts.sol
+++ smart-contracts/SmokeSignal/contracts/Scripts.sol
@@ -4,24 +4,36 @@ pragma experimental ABIEncoderV2;
 import "./SmokeSignal.sol";
 
 contract Scripts {
-    function getBulkAccounting(SmokeSignal smokeSignal, bytes32[] calldata messageIds)
+    function getBulkAccounting(
+        SmokeSignal smokeSignal,
+        bytes32[] calldata messageIds
+    )
         external
         view
-        returns
-            (address[] memory firstAuthorArray,
-             uint[] memory nativeBurnedArray,
-             uint[] memory dollarsBurnedArray,
-             uint[] memory nativeTippedArray,
-             uint[] memory dollarsTippedArray
-            )
-        {
-            for (uint i=0; i<messageIds.length; i++) {
-                (firstAuthorArray[i],
-                 nativeBurnedArray[i],
-                 dollarsBurnedArray[i],
-                 nativeTippedArray[i],
-                 dollarsTippedArray[i]
-                ) = smokeSignal.storedMessageData(messageIds[i]);
-            }
+        returns (
+            address[] memory firstAuthorArray,
+            uint256[] memory nativeBurnedArray,
+            uint256[] memory dollarsBurnedArray,
+            uint256[] memory nativeTippedArray,
+            uint256[] memory dollarsTippedArray
+        )
+    {
+        uint256 size = messageIds.length;
+
+        firstAuthorArray = new address[](size);
+        nativeBurnedArray = new uint256[](size);
+        dollarsBurnedArray = new uint256[](size);
+        nativeTippedArray = new uint256[](size);
+        dollarsTippedArray = new uint256[](size);
+
+        for (uint256 i = 0; i < size; i++) {
+            (
+                firstAuthorArray[i],
+                nativeBurnedArray[i],
+                dollarsBurnedArray[i],
+                nativeTippedArray[i],
+                dollarsTippedArray[i]
+            ) = smokeSignal.storedMessageData(messageIds[i]);
         }
+    }
 }
